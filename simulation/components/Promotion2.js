6066158bb885702d80728090a01e4f46a1c4f1f4
Laurelia <55199147+Laurelia@users.noreply.github.com>
2019-10-04 09:33:31 -0500

    Create Promotion2.js
    Duplicating promotion component to allow religious choices

---
+++ simulation/components/Promotion2.js
@@ -0,0 +1,100 @@
+function Promotion2() {}
+
+Promotion2.prototype.Schema =
+	"<element name='Entity'>" +
+		"<text/>" +
+	"</element>" +
+	"<element name='RequiredXp'>" +
+		"<data type='positiveInteger'/>" +
+	"</element>";
+
+Promotion2.prototype.Init = function()
+{
+	this.currentXp = 0;
+};
+
+Promotion2.prototype.GetRequiredXp = function()
+{
+	return ApplyValueModificationsToEntity("Promotion2/RequiredXp", +this.template.RequiredXp, this.entity);
+};
+
+Promotion2.prototype.GetCurrentXp = function()
+{
+	return this.currentXp;
+};
+
+Promotion2.prototype.GetPromotedTemplateName = function()
+{
+	return this.template.Entity;
+};
+
+Promotion2.prototype.Promote = function(promotedTemplateName)
+{
+	// If the unit is dead, don't promote it
+	let cmpHealth = Engine.QueryInterface(this.entity, IID_Health);
+	if (cmpHealth && cmpHealth.GetHitpoints() == 0)
+	{
+		this.promotedUnitEntity = INVALID_ENTITY;
+		return;
+	}
+
+	// Save the entity id.
+	this.promotedUnitEntity = ChangeEntityTemplate(this.entity, promotedTemplateName);
+
+	let cmpPosition = Engine.QueryInterface(this.promotedUnitEntity, IID_Position);
+	let cmpUnitAI = Engine.QueryInterface(this.promotedUnitEntity, IID_UnitAI);
+
+	if (cmpPosition && cmpPosition.IsInWorld() && cmpUnitAI)
+		cmpUnitAI.Cheer();
+};
+
+Promotion2.prototype.IncreaseXp = function(amount)
+{
+	// if the unit was already promoted, but is waiting for the engine to be destroyed
+	// transfer the gained xp to the promoted unit if applicable
+	if (this.promotedUnitEntity)
+	{
+		let cmpPromotion2 = Engine.QueryInterface(this.promotedUnitEntity, IID_Promotion2);
+		if (cmpPromotion2)
+			cmpPromotion2.IncreaseXp(amount);
+		return;
+	}
+
+	this.currentXp += +(amount);
+	var requiredXp = this.GetRequiredXp();
+
+	if (this.currentXp >= requiredXp)
+	{
+		var cmpTemplateManager = Engine.QueryInterface(SYSTEM_ENTITY, IID_TemplateManager);
+		var playerID = QueryOwnerInterface(this.entity, IID_Player).GetPlayerID();
+		this.currentXp -= requiredXp;
+		var promotedTemplateName = this.GetPromotedTemplateName();
+		// check if we can upgrade a second time (or even more)
+		while (true)
+		{
+			var template = cmpTemplateManager.GetTemplate(promotedTemplateName);
+			if (!template.Promotion2)
+				break;
+			requiredXp = ApplyValueModificationsToTemplate("Promotion2/RequiredXp", +template.Promotion2.RequiredXp, playerID, template);
+			// compare the current xp to the required xp of the promoted entity
+			if (this.currentXp < requiredXp)
+				break;
+			this.currentXp -= requiredXp;
+			promotedTemplateName = template.Promotion2.Entity;
+		}
+		this.Promote(promotedTemplateName);
+		let cmpPromotion2 = Engine.QueryInterface(this.promotedUnitEntity, IID_Promotion2);
+		if (cmpPromotion2)
+			cmpPromotion2.IncreaseXp(this.currentXp);
+	}
+
+	Engine.PostMessage(this.entity, MT_ExperienceChanged, {});
+};
+
+Promotion2.prototype.OnValueModification = function(msg)
+{
+	if (msg.component == "Promotion2")
+		this.IncreaseXp(0);
+};
+
+Engine.RegisterComponentType(IID_Promotion2, "Promotion2", Promotion2);
